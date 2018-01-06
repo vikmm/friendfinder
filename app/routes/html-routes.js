ec84d585d0c990b7154a8e99993200744d7fdd7b
Victor Montemayor <vikmonte@gmail.com>
2018-01-06 09:52:57 -0600

    final load

--- app/routes/html-routes.js
+++ app/routes/html-routes.js
@@ -1,5 +1,5 @@
 
-//dependencies
+
 var path = require('path');
 
 module.exports = function(app){
@@ -8,7 +8,7 @@ module.exports = function(app){
 		res.sendFile(path.join(__dirname + '/../public/survey.html'));
 	});
 
-	// If no matching route is found default to home
+	
 	app.use(function(req, res){
 		res.sendFile(path.join(__dirname + '/../public/home.html'));
 	});
